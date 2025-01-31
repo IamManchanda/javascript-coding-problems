3875af676e5a726fe7e64b063db16f7cfbac4e63
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-01-31 12:05:22 +0530

    test cases refactor

--- 002-algoexpert/001-two-number-sum/__shared__/shared.test.js
+++ 002-algoexpert/001-two-number-sum/__shared__/shared.test.js
@@ -11,10 +11,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [-1, 11] // in any order
 } */
-
   test('Test Case 1', () => {
-    const array = [3, 5, -4, 8, 11, 1, -1, 6];
-    const targetSum = 10;
+    const { array, targetSum } = {
+      array: [3, 5, -4, 8, 11, 1, -1, 6],
+      targetSum: 10,
+    };
 
     const expected = [-1, 11];
     const result = solution.twoNumberSum(array, targetSum);
@@ -28,10 +29,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [4, 6] // in any order
 } */
-
   test('Test Case 2', () => {
-    const array = [4, 6];
-    const targetSum = 10;
+    const { array, targetSum } = {
+      array: [4, 6],
+      targetSum: 10,
+    };
 
     const expected = [4, 6];
     const result = solution.twoNumberSum(array, targetSum);
@@ -45,10 +47,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [1, 4] // in any order
 }*/
-
   test('Test Case 3', () => {
-    const array = [4, 6, 1];
-    const targetSum = 5;
+    const { array, targetSum } = {
+      array: [4, 6, 1],
+      targetSum: 5,
+    };
 
     const expected = [1, 4];
     const result = solution.twoNumberSum(array, targetSum);
@@ -62,10 +65,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [-3, 6] // in any order
 } */
-
   test('Test Case 4', () => {
-    const array = [4, 6, 1, -3];
-    const targetSum = 3;
+    const { array, targetSum } = {
+      array: [4, 6, 1, -3],
+      targetSum: 3,
+    };
 
     const expected = [-3, 6];
     const result = solution.twoNumberSum(array, targetSum);
@@ -79,10 +83,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [8, 9] // in any order
 } */
-
   test('Test Case 5', () => {
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const targetSum = 17;
+    const { array, targetSum } = {
+      array: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      targetSum: 17,
+    };
 
     const expected = [8, 9];
     const result = solution.twoNumberSum(array, targetSum);
@@ -96,10 +101,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [3, 15] // in any order
 } */
-
   test('Test Case 6', () => {
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15];
-    const targetSum = 18;
+    const { array, targetSum } = {
+      array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
+      targetSum: 18,
+    };
 
     const expected = [3, 15];
     const result = solution.twoNumberSum(array, targetSum);
@@ -113,10 +119,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [-5, 0] // in any order
 } */
-
   test('Test Case 7', () => {
-    const array = [-7, -5, -3, -1, 0, 1, 3, 5, 7];
-    const targetSum = -5;
+    const { array, targetSum } = {
+      array: [-7, -5, -3, -1, 0, 1, 3, 5, 7],
+      targetSum: -5,
+    };
 
     const expected = [-5, 0];
     const result = solution.twoNumberSum(array, targetSum);
@@ -130,10 +137,11 @@ export const sharedTests = (getSolution) => {
 }, {
   "result": [-47, 210] // in any order
 } */
-
   test('Test Case 8', () => {
-    const array = [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47];
-    const targetSum = 163;
+    const { array, targetSum } = {
+      array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
+      targetSum: 163,
+    };
 
     const expected = [-47, 210];
     const result = solution.twoNumberSum(array, targetSum);
@@ -145,12 +153,13 @@ export const sharedTests = (getSolution) => {
   "array": [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
   "targetSum": 164
 }, {
-  "result": [] // in any order
+  "result": []
 } */
-
   test('Test Case 9', () => {
-    const array = [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47];
-    const targetSum = 164;
+    const { array, targetSum } = {
+      array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
+      targetSum: 164,
+    };
 
     const expected = [];
     const result = solution.twoNumberSum(array, targetSum);
@@ -162,12 +171,13 @@ export const sharedTests = (getSolution) => {
   "array": [3, 5, -4, 8, 11, 1, -1, 6],
   "targetSum": 15
 }, {
-  "result": [] // in any order
+  "result": []
 } */
-
   test('Test Case 10', () => {
-    const array = [3, 5, -4, 8, 11, 1, -1, 6];
-    const targetSum = 15;
+    const { array, targetSum } = {
+      array: [3, 5, -4, 8, 11, 1, -1, 6],
+      targetSum: 15,
+    };
 
     const expected = [];
     const result = solution.twoNumberSum(array, targetSum);
@@ -179,12 +189,13 @@ export const sharedTests = (getSolution) => {
   "array": [14],
   "targetSum": 15
 }, {
-  "result": [] // in any order
+  "result": []
 } */
-
   test('Test Case 11', () => {
-    const array = [14];
-    const targetSum = 15;
+    const { array, targetSum } = {
+      array: [14],
+      targetSum: 15,
+    };
 
     const expected = [];
     const result = solution.twoNumberSum(array, targetSum);
@@ -196,12 +207,13 @@ export const sharedTests = (getSolution) => {
   "array": [15],
   "targetSum": 15
 }, {
-  "result": [] // in any order
+  "result": []
 } */
-
   test('Test Case 12', () => {
-    const array = [15];
-    const targetSum = 15;
+    const { array, targetSum } = {
+      array: [15],
+      targetSum: 15,
+    };
 
     const expected = [];
     const result = solution.twoNumberSum(array, targetSum);

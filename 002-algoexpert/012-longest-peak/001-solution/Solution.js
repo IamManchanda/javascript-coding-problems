a8a5ef7ee8241ba669c28378e3bf157fc92f88cc
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-03 22:59:58 +0530

    Longest Peak variable updates

--- 002-algoexpert/012-longest-peak/001-solution/Solution.js
+++ 002-algoexpert/012-longest-peak/001-solution/Solution.js
@@ -10,7 +10,7 @@
 class Solution {
   longestPeak(array) {
     const n = array.length;
-    let longestPeak = 0, i = 1;
+    let longestPeakLength = 0, i = 1;
 
     while (i < n - 1) {
       const isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
@@ -26,14 +26,14 @@ class Solution {
       let r = i + 2;
       while (r <= n - 1 && array[r] < array[r - 1]) r++;
 
-      // currentPeak = (r - 1) - (l + 1) + 1 => r - l - 1
-      const currentPeak = r - l - 1; 
-      longestPeak = Math.max(longestPeak, currentPeak);
+      // currentPeakLength = (r - 1) - (l + 1) + 1 => r - l - 1
+      const currentPeakLength = r - l - 1; 
+      longestPeakLength = Math.max(longestPeakLength, currentPeakLength);
 
       i = r; // Skip to the next potential peak
     }
 
-    return longestPeak;
+    return longestPeakLength;
   }
 }
 

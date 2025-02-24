c8f218f2de74c6ff5beff8b69a1808ef8a3518e1
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-24 19:53:06 +0530

    Add debug for: - tournamentWinner - nonConstructibleChange

---
+++ 002-algoexpert/004-tournament-winner/002-solution/Debug.js
@@ -0,0 +1,5 @@
+import Solution from "./Solution.js";
+
+const solution = new Solution();
+const result = solution.tournamentWinner([["HTML","C#"],["C#","Python"],["Python","HTML"]]);
+console.log(result); // "Python"
---
+++ 002-algoexpert/005-non-constructible-change/001-solution/Debug.js
@@ -0,0 +1,5 @@
+import Solution from "./Solution.js";
+
+const solution = new Solution();
+const result = solution.nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]);
+console.log(result); // 20

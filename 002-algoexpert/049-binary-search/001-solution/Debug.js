4f41b274d59041ea159077ef0cb13d1052afabaa
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-24 14:33:02 +0530

    Add debug for binarySearch

---
+++ 002-algoexpert/049-binary-search/001-solution/Debug.js
@@ -0,0 +1,5 @@
+import Solution from "./Solution.js";
+
+const solution = new Solution();
+const result = solution.binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33);
+console.log(result); // 3

c0aea4e3b1fc28827545f5433fc87092805ed780
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-24 18:18:17 +0530

    Add debug for isMonotonic

---
+++ 002-algoexpert/010-monotonic-array/001-solution/Debug.js
@@ -0,0 +1,5 @@
+import Solution from "./Solution.js";
+
+const solution = new Solution();
+const result = solution.isMonotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]);
+console.log(result); // true

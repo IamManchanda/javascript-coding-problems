a8510051711799edb4a6325ba72581399bf15a14
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-24 15:27:01 +0530

    add debug for kadanesAlgorithm

---
+++ 002-algoexpert/047-kadane's-algorithm/001-solution/Debug.js
@@ -0,0 +1,5 @@
+import Solution from "./Solution.js";
+
+const solution = new Solution();
+const result = solution.kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]);
+console.log(result); // 19

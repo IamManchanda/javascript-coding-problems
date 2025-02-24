b2a0cb4e6bfa425bf9dcdf271cb6718c5329553a
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-24 14:21:34 +0530

    Add debug for longestSubstringWithoutDuplication

---
+++ 002-algoexpert/050-longest-substring-without-duplication/001-solution/Debug.js
@@ -0,0 +1,5 @@
+import Solution from "./Solution.js";
+
+const solution = new Solution();
+const result = solution.longestSubstringWithoutDuplication("clementisacap");
+console.log(result); // mentisac

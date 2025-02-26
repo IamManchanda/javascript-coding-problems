8fec76d2bc2a2110ec1c7f17c5647e249f200176
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-26 17:59:51 +0530

    Debug updates

--- 002-algoexpert/003-sorted-squared-array/002-solution/Debug.js
+++ 002-algoexpert/003-sorted-squared-array/002-solution/Debug.js
@@ -1,5 +1,5 @@
 import Solution from "./Solution.js";
 
 const solution = new Solution();
-const result = solution.sortedSquaredArray([1, 2, 3, 5, 6, 8, 9]);
-console.log(result); // [1, 4, 9, 25, 36, 64, 81]
+const result = solution.sortedSquaredArray([-10, -5, 0, 5, 10]);
+console.log(result); // [0, 25, 25, 100, 100]

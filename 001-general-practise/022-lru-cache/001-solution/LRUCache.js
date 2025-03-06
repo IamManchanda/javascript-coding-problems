db07afb9dce3cedb6f20be8b256d8a853d49dfa9
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-03-06 19:24:16 +0530

    LRU Cache updates

--- 001-general-practise/022-lru-cache/001-solution/LRUCache.js
+++ 001-general-practise/022-lru-cache/001-solution/LRUCache.js
@@ -1,13 +1,9 @@
-/**
- * LRU Cache
- */
-
 class Node {
-  constructor(key, value, prev = null, next = null) {
+  constructor(key, value) {
     this.key = key;
     this.value = value;
-    this.prev = prev;
-    this.next = next;
+    this.prev = null;
+    this.next = null;
   }
 }
 
@@ -26,44 +22,45 @@ class LRUCache {
     }
 
     const node = this.map.get(key);
-    this.#removeNode(node);
-    node.prev = null;
-    node.next = this.head;
-
-    if (this.head !== null) {
-      this.head.prev = node;
-    }
-
-    this.head = node;
-
-    if (this.tail === null) {
-      this.tail = node;
-    }
+    this.#moveToHead(node);
 
     return node.value;
   }
 
   put(key, value) {
-    if (this.length === this.capacity && !this.map.has(key)) {
-      this.#removeNode(this.tail);
-      this.length--;
-    }
-
     if (this.map.has(key)) {
-      this.#removeNode(this.map.get(key));
-      this.length--;
+      const existingNode = this.map.get(key);
+      existingNode.value = value;
+      this.#moveToHead(existingNode);
+      
+      return;
     }
 
-    const node = new Node(key, value, null, this.head);
-    this.map.set(key, node);
+    if (this.length === this.capacity) {
+      this.#removeNode(this.tail);
+    }
 
-    if (this.head !== null) {
+    const node = new Node(key, value);
+    this.#addNode(node);
+    this.map.set(key, node);
+  }
+
+  #moveToHead(node) {
+    this.#removeNode(node);
+    this.#addNode(node);
+  }
+
+  #addNode(node) {
+    node.next = this.head;
+    node.prev = null;
+
+    if (this.head) {
       this.head.prev = node;
     }
-
+    
     this.head = node;
 
-    if (this.tail === null) {
+    if (!this.tail) {
       this.tail = node;
     }
 
@@ -71,15 +68,11 @@ class LRUCache {
   }
 
   #removeNode(node) {
-    if (node === null) {
-      return;
-    }
-
-    if (node.prev !== null) {
+    if (node.prev) {
       node.prev.next = node.next;
     }
 
-    if (node.next !== null) {
+    if (node.next) {
       node.next.prev = node.prev;
     }
 
@@ -90,13 +83,16 @@ class LRUCache {
     if (node === this.tail) {
       this.tail = node.prev;
     }
+
+    this.map.delete(node.key);
+    this.length--;
   }
 
   debug() {
-    let current = this.head;
     const arr = [];
+    let current = this.head;
 
-    while (current !== null) {
+    while (current) {
       arr.push(current);
       current = current.next;
     }
